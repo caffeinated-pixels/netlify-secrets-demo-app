f3c25dc49f05e8c1b5a1834676a321d11a1b6c25
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-08-10 13:11:31 -0400

    Add netlify function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
-  const [todoId, setTodoId] = useState(1);
-  const [todo, setTodo] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [todoId, setTodoId] = useState(1)
+  const [todo, setTodo] = useState('')
+  const [loading, setLoading] = useState(false)
 
   function getNewTodo() {
-    setTodoId((todoId) => (todoId === 20 ? 1 : todoId + 1));
+    setTodoId(todoId => (todoId === 20 ? 1 : todoId + 1))
   }
 
   useEffect(() => {
     async function fetchTodo() {
-      const url = `${process.env.REACT_APP_TODO_BASE_URL}/${todoId}`;
+      const url = `/.netlify/functions/todo?id=${todoId}`
+
       try {
-        setLoading(true);
-        const todo = await fetch(url).then((res) => res.json());
-        setTodo(todo.title);
+        setLoading(true)
+        const todo = await fetch(url).then(res => res.json())
+        setTodo(todo.title)
       } catch (err) {
-        console.log(err);
+        console.log(err)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
     }
-    fetchTodo();
-  }, [todoId]);
+    fetchTodo()
+  }, [todoId])
 
   return (
     <div className="App">
       <p>
         <button onClick={getNewTodo}> Get another todo </button>
       </p>
-      <p>{loading ? "Loading..." : todo}</p>
+      <p>{loading ? 'Loading...' : todo}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
